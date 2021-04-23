2bd983a035479365f3dea66e454a776b09e4b2d9
ModuloHeart <58439700+ModuloHeart@users.noreply.github.com>
2021-04-23 11:36:09 +0200

    Test

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Hello !!</Text>
+          <StatusBar style="auto"/>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
