ad1a014f289959d58932885ec252a98b81ac6236
Algonight <benjamin.fournet99@gmail.com>
2021-05-04 20:12:33 +0200

    Update App.js to test api

--- front/App.js
+++ front/App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = {
+    name : ""
+  }
+
+  componentDidMount() {
+    axios.get('localhost:3000/api/stuff')
+      .then(res => {
+        const name = res.data;
+        this.setState({name});
+      })
+  }
+
   render() {
+    const {name} = this.state;
     return (
         <View style={styles.container}>
-          <Text style={{color: '#fff'}}>Hello !!!!!</Text>
+          <Text style={{color: '#fff'}}>Hello Joris Bitch !!!!!</Text>
+          <Text style={{color: 'red'}}>C'est moi {name} le boss mtn</Text>
           <StatusBar style="auto"/>
         </View>
     );
