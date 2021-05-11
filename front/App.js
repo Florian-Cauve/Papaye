f0388b01cdcb5e13ed4a559e6f3b700abb5ce6ec
JorisBaudchon <55876397+JorisBaudchon@users.noreply.github.com>
2021-05-11 18:57:01 +0200

    [UPDATE] création fichiers +
    création App.js ; package.json ; package-lock.json ; server.js ; app.js
    
    début fonction appel API

--- front/App.js
+++ front/App.js
@@ -6,23 +6,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
 
   state = {
-    name : ""
+    name : []
   }
 
   componentDidMount() {
-    axios.get('localhost:3000/api/stuff')
+    axios.get('http://localhost:3000/api/stuff')
       .then(res => {
-        const name = res.data;
-        this.setState({name});
-      })
+        console.log("result +" + res)
+      }).catch(error => {console.log("error +" + error)}) 
   }
 
-  render() {
+  render() { 
     const {name} = this.state;
     return (
         <View style={styles.container}>
-          <Text style={{color: '#fff'}}>Hello Joris Bitch !!!!!</Text>
-          <Text style={{color: 'red'}}>C'est moi {name} le boss mtn</Text>
           <StatusBar style="auto"/>
         </View>
     );
