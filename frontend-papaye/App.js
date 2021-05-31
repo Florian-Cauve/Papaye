925e3d143b299bd5e2a9656a2a16a6665bcaa0e7
JorisBaudchon <55876397+JorisBaudchon@users.noreply.github.com>
2021-05-31 11:54:26 +0200

    tests taoilwind

--- frontend-papaye/App.js
+++ frontend-papaye/App.js
@@ -1,43 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import axios from 'axios';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Text, View,SafeAreaView } from 'react-native';
+import tailwind from 'tailwind-rn';
 
-export default class App extends React.Component {
-
-  state = {
-    name : ""
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:8082/api/users/test')
-      .then(res => {
-        console.log(res);
-        const name = res.data;
-        this.setState({name});
-      })
-  }
-
-  render() {
-    const {name} = this.state;
-    return (
-        <View style={styles.container}>
-          <Text style={{color: '#fff'}}>Hello Joris Bitch !!!!!</Text>
-          <Button variant="success">Success</Button>{' '}
-          <Text style={{color: '#fff'}}>C'est moi {name} le boss mtn</Text>
-          <StatusBar style="auto"/>
-        </View>
-    );
-  }
+export default function App() {
+  return(
+    <SafeAreaView style={tailwind('flex-1 justify-center items-center')}>
+      <View style={tailwind('bg-blue-500 py-3 px-5 rounded-full')}>
+        <Text style={tailwind('text-white font-bold')}>
+          Hello there
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6699ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
