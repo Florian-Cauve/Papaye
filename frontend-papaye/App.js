fb8e8432ff7700ce6dcc853ab9f9b22d4e0013f7
Fathi <fathi.kadir.a@gmail.com>
2021-06-05 13:36:19 +0200

    changing App Route balise

--- frontend-papaye/App.js
+++ frontend-papaye/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import HomePage from "./src/components/HomePage.js";
 import { NativeRouter, Route } from "react-router-native";
 
@@ -8,12 +8,13 @@ import SignUp from "./src/components/SignUp.js";
 import News from "./src/components/News.js";
 
 export default function App() {
+
   return(
     <NativeRouter>
-      <Route path="/" exact component={HomePage}></Route>
-      <Route path="/login" component={Login}></Route>
-      <Route path="/signup" component={SignUp}></Route>
-      <Route path="/news" component={News}></Route>
+      <Route path="/" exact component={HomePage}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={SignUp}/>
+      <Route path="/news" component={News}/>
     </NativeRouter>
   )
 }
