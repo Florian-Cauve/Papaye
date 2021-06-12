d7745dff088f01c22881d93e01c49172b7a2524a
Florian-Cauve <florian.cauve@outlook.com>
2021-06-12 13:45:14 +0200

    Ajout de la partie DoExercise

--- frontend-papaye2.0/src/App.tsx
+++ frontend-papaye2.0/src/App.tsx
@@ -12,6 +12,7 @@ import MyTrainings from "./components/Trainings/MyTrainings";
 import Profile from "./components/Profile/Profile";
 import Receipe from './components/Receipes/Receipe';
 import Training from './components/Trainings/Training';
+import DoExercise from './components/Trainings/DoExercise';
 
 function App() {
   const [userConnected, setUserConnected] = useState<boolean>(false)
@@ -41,7 +42,7 @@ function App() {
             <Route path="/trainings" component={MyTrainings}/>
             <Route path="/training/:id" component={Training}/>
             {/*<Route path="/open_training/:id_program" component={OpenTraining}/>*/}
-            <Route path="/do_exercise/:id_program" component={DoExercise}/>
+            <Route path="/do_exercise/:id" component={DoExercise}/>
             <Route path="/account" component={Profile}/>
         </Switch>
         )}
