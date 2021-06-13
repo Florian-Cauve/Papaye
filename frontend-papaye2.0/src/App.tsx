34ce767e4a3e16f85d33ec7be19c69186a19d30b
Algonight <benjamin.fournet99@gmail.com>
2021-06-13 18:48:08 +0200

    [UPDATE] add session managing

--- frontend-papaye2.0/src/App.tsx
+++ frontend-papaye2.0/src/App.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from "./components/HomePage/HomePage";
 import Login from "./components/Authentication/Login";
@@ -19,37 +19,44 @@ function App() {
   const [userConnected, setUserConnected] = useState<boolean>(false)
 
   useEffect(() => {
-      const currentUserId:string | null  = (localStorage.getItem("id") !== "") ? localStorage.getItem("id") : null;
-      setUserConnected(currentUserId !== null && currentUserId !== undefined);
-      console.log(currentUserId)
-      if(currentUserId === undefined){
-          localStorage.removeItem("id")
-          document.location.href = "/"
+    const currentUserId: string | null = (localStorage.getItem("id") !== "") ? localStorage.getItem("id") : null;
+    setUserConnected(currentUserId !== null && currentUserId !== undefined);
+    console.log(currentUserId)
+    console.log(document.location);
+    
+    if(currentUserId != null){
+      if(document.location.pathname != "/social"){
+        document.location.href="/social"
       }
+    }else{
+      if(document.location.pathname != "/"){
+        document.location.href="/"
+      }
+    }
   }, [])
 
   return (
     <div className="App">
       <Router>
         {!userConnected ? (
-            <Switch>
-              <Route path="/" exact component={HomePage}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/signup" component={SignUp}/>
-            </Switch>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
         ) : (
-        <Switch>
-            <Route path="/social" component={DisplayPost}/>
-            <Route path="/addPost" component={AddSocialPost}/>
-            <Route path="/receipes" component={MyReceipes}/>
-            <Route path="/addReceipe" component={AddReceipe}/>
-            <Route path="/receipe/:id" exact component={Receipe}/>
-            <Route path="/receipe/update/:id" component={UpdateReceipe}/>
-            <Route path="/trainings" component={MyTrainings}/>
-            <Route path="/training/:id" component={Training}/>
-            <Route path="/do_exercise/:id" component={DoExercise}/>
-            <Route path="/account" component={Profile}/>
-        </Switch>
+          <Switch>
+            <Route path="/social" component={DisplayPost} />
+            <Route path="/addPost" component={AddSocialPost} />
+            <Route path="/receipes" component={MyReceipes} />
+            <Route path="/addReceipe" component={AddReceipe} />
+            <Route path="/receipe/:id" exact component={Receipe} />
+            <Route path="/receipe/update/:id" component={UpdateReceipe} />
+            <Route path="/trainings" component={MyTrainings} />
+            <Route path="/training/:id" component={Training} />
+            <Route path="/do_exercise/:id" component={DoExercise} />
+            <Route path="/account" component={Profile} />
+          </Switch>
         )}
       </Router>
     </div>
